deca238226e0b42e6e5e1550e2a9e853fa2158e5
JereKoskela <jere.koskela@newcastle.ac.uk>
2024-09-09 13:03:55 +0100

    Visualisation formatting

--- plots.R
+++ plots.R
@@ -8,20 +8,20 @@ pdf(paste0("dose_x_", n_str, ".pdf"))
 plot(data[,1], data[,2], type="l", xlab = "Depth", ylab = "Dose", cex.lab=1.3, cex.axis=1.3, xlim=c(0,80))
 dev.off()
 
-scale_resolution = 2
+scale_resolution = 3
 data = read.table(paste0("./test_xy_", n_str, ".txt"))
 data[,3] = data[,3] / n
 pdf(paste0("dose_xy_", n_str, ".pdf"))
 offset = min(log10(data[,3]) * scale_resolution) - 1
 colvec = round(log10(data[,3]) * scale_resolution - offset)
 plot(data[,1], data[,2], col=viridis(max(colvec))[colvec], xlab = "Depth", ylab = "Height", cex.lab=1.3, cex.axis=1.3, cex = 0.1, pch=20, xlim=c(0,80), ylim=c(-5,5))
-legend("topleft", legend=round(min(colvec):max(colvec) + offset) / scale_resolution, pch=15, col=viridis(max(colvec)), cex=1.2, ncol=2)
+legend("topleft", legend=round((min(colvec):max(colvec) + offset) / scale_resolution, 1), pch=15, col=viridis(max(colvec)), cex=1.2, ncol=3)
 dev.off()
 png(paste0("dose_xy_", n_str, ".png"))
 offset = min(log10(data[,3]) * scale_resolution) - 1
 colvec = round(log10(data[,3]) * scale_resolution - offset)
 plot(data[,1], data[,2], col=viridis(max(colvec))[colvec], xlab = "Depth", ylab = "Height", cex.lab=1.3, cex.axis=1.3, cex = 0.1, pch=20, xlim=c(0,80), ylim=c(-5,5))
-legend("topleft", legend=round(min(colvec):max(colvec) + offset) / scale_resolution, pch=15, col=viridis(max(colvec)), cex=1.2, ncol=2)
+legend("topleft", legend=round((min(colvec):max(colvec) + offset) / scale_resolution, 1), pch=15, col=viridis(max(colvec)), cex=1.2, ncol=3)
 dev.off()
 
 
@@ -31,11 +31,11 @@ pdf(paste0("dose_z=0_", n_str, ".pdf"))
 offset = min(log10(data[,3]) * scale_resolution) - 1
 colvec = round(log10(data[,3]) * scale_resolution - offset)
 plot(data[,1], data[,2], col=viridis(max(colvec))[colvec], xlab = "Depth", ylab = "Height", cex.lab=1.3, cex.axis=1.3, cex = 0.1, pch=20, ylim=c(-5, 5), xlim=c(0,80))
-legend("topleft", legend=round(min(colvec):max(colvec) + offset) / scale_resolution, pch=15, col=viridis(max(colvec)), cex=1.2, ncol = 2)
+legend("topleft", legend=round((min(colvec):max(colvec) + offset) / scale_resolution, 1), pch=15, col=viridis(max(colvec)), cex=1.2, ncol = 3)
 dev.off()
 png(paste0("dose_z=0_", n_str, ".png"))
 offset = min(log10(data[,3]) * scale_resolution) - 1
 colvec = round(log10(data[,3]) * scale_resolution - offset)
 plot(data[,1], data[,2], col=viridis(max(colvec))[colvec], xlab = "Depth", ylab = "Height", cex.lab=1.3, cex.axis=1.3, cex = 0.1, pch=20, ylim=c(-5, 5), xlim=c(0,80))
-legend("topleft", legend=round(min(colvec):max(colvec) + offset) / scale_resolution, pch=15, col=viridis(max(colvec)), cex=1.2, ncol = 2)
+legend("topleft", legend=round((min(colvec):max(colvec) + offset) / scale_resolution, 1), pch=15, col=viridis(max(colvec)), cex=1.2, ncol = 3)
 dev.off()
