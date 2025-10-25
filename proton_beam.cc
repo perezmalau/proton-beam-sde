897a4ea079e1169315a184172248814a4e6401db
JereKoskela <jere.koskela@newcastle.ac.uk>
2025-10-25 14:15:10 +0100

    Clang formatting

--- proton_beam.cc
+++ proton_beam.cc
@@ -222,12 +222,13 @@ struct proton_path {
       x[ix][2] = x[ix - 1][2] - time_step * (cos(v0) + cos(v1)) / 2;
     }
     double bethe_block_update = mat.bethe_bloch(energy[ix - 1]) * time_step;
-    energy[ix] =
-        energy[ix - 1] -
-        fmin(fmax(bethe_block_update +
-                 sqrt(time_step) * mat.energy_straggling_sd(energy[ix - 1]) *
-                     gsl_ran_gaussian_ziggurat(gen, 1),
-             0),2*bethe_block_update);
+    energy[ix] = energy[ix - 1] -
+                 fmin(fmax(bethe_block_update +
+                               sqrt(time_step) *
+                                   mat.energy_straggling_sd(energy[ix - 1]) *
+                                   gsl_ran_gaussian_ziggurat(gen, 1),
+                           0),
+                      2 * bethe_block_update);
     energy[ix] = fmax(energy[ix], 0);
     s[ix] = energy[ix - 1] - energy[ix];
     ix++;
