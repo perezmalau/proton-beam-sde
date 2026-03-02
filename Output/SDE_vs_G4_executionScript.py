ad2565416c6643aa055d114c1e2970709894ac46
Maria Perez-Lara <maria.lara.19@ucl.ac.uk>
2026-03-02 13:53:32 +0000

    Change figure names and order, change dose to Gy, add material boundaries in 1D plots

--- Output/SDE_vs_G4_executionScript.py
+++ Output/SDE_vs_G4_executionScript.py
@@ -2,10 +2,10 @@
 Calculations and plots for the SDE article
 Author: Maria L. Perez-Lara
 Returns figures comparing SDE vs Geant4 results in a single specified phantom geometry:
-    Figures 1 and 4: 2D slices in z for 100 MeV and 150 MeV
-    Figures 2 and 5: Lateral profiles at different depths for 100 and 150 MeV
-    Figures 3 and 6: Central slice of percentage dose difference array for 100 and 150 MeV
-    Figures 7 and 8: 1D slices and projections of the Bragg peaks
+    Figures a and b: 1D slices and projections of the Bragg peaks
+    Figures c and d: Lateral profiles at different depths for 100 and 150 MeV
+    Figures e and g: 2D slices in z for 100 MeV and 150 MeV
+    Figures f and h: Central slice of percentage dose difference array for 100 and 150 MeV
     Other figures: In case comparePhysics is set to True
 Also prints in the terminal the following values:
     Gamma pass rates using the DD, DTA and TH values specified
@@ -38,12 +38,13 @@ N_primaries = "1E6"
 suffix = "_water"
 energy1 = 100  # MeV
 energy2 = 150  # MeV
-comparePhysics = False  # Set to true if data from BERT and EMY are available to plot comparison between phys lists
+comparePhysics = False # Set to true if data from BERT and EMY are available to plot comparison between phys lists
 voxel_volume = 0.001  # cm3
 dd = 2
 dta = 0.5
 th = 1
 fig_extension = ".eps"
+MeV_g_to_Gy = 1.60218e-10
 
 # Adjust parameters according to case:
 if suffix == "_slab":
@@ -55,48 +56,41 @@ if suffix == "_slab":
     )  # add lung
     cuts_100 = [10, 25, 40, 85]
     cuts_150 = [10, 25, 40, 165]
+    material_boundaries = [2, 3, 5]
 elif suffix == "_insert":
     density_matrix = include_new_material(
         np.ones((200, 200, 200)), tmin=30, tmax=50, rho=1.45, ycut=100
     )  # add bone
     cuts_100 = [15, 40, 69, 76]
     cuts_150 = [15, 40, 150, 157]
+    material_boundaries = [3, 5]
 else:
     density_matrix = np.ones((200, 200, 200))
     cuts_100 = [30, 50, 75]
     cuts_150 = [50, 100, 150]
+    material_boundaries = None
 
 # Data import
-g4_dose1 = retrieve_g4_output(f"G4_{N_primaries}_{energy1}MeV{suffix}.root") / (
-    float(N_primaries) * density_matrix * voxel_volume
+g4_dose1 = MeV_g_to_Gy * retrieve_g4_output(f"G4_{N_primaries}_{energy1}MeV{suffix}.root") / (
+    density_matrix * voxel_volume
 )
-sde_dose1 = retrieve_sde_output(f"SDE_{N_primaries}_{energy1}MeV{suffix}.txt") / (
-    float(N_primaries) * density_matrix * voxel_volume
-)
-
-g4_dose2 = retrieve_g4_output(f"G4_{N_primaries}_{energy2}MeV{suffix}.root") / (
-    float(N_primaries) * density_matrix * voxel_volume
-)
-sde_dose2 = retrieve_sde_output(f"SDE_{N_primaries}_{energy2}MeV{suffix}.txt") / (
-    float(N_primaries) * density_matrix * voxel_volume
+sde_dose1 = MeV_g_to_Gy * retrieve_sde_output(f"SDE_{N_primaries}_{energy1}MeV{suffix}.txt") / (
+    density_matrix * voxel_volume
 )
 
-# Individual energy plots - energy 1
-f1 = plot_slice(
-    g4_dose1,
-    sde_dose1,
-    "z",
-    minval=-5,
-    xmin=0,
-    xmax=9.5,
-    ymin=-5,
-    ymax=5,
-    zmin=-5,
-    zmax=5,
+g4_dose2 = MeV_g_to_Gy * retrieve_g4_output(f"G4_{N_primaries}_{energy2}MeV{suffix}.root") / (
+    density_matrix * voxel_volume
 )
-f1.savefig(f"2DSlice_z_{energy1}MeV{suffix}{fig_extension}")
+sde_dose2 = MeV_g_to_Gy * retrieve_sde_output(f"SDE_{N_primaries}_{energy2}MeV{suffix}.txt") / (
+    density_matrix * voxel_volume
+)
 
-f2 = plot_lateral_profiles(
+# Comparative plots - 1D
+fa, fb = compare_bragg_peaks(sde_dose1, g4_dose1, energy1, sde_dose2, g4_dose2, energy2,
+                             material_boundaries=material_boundaries)
+
+# Lateral profiles
+fc = plot_lateral_profiles(
     g4_dose1,
     sde_dose1,
     xmin=0,
@@ -109,9 +103,36 @@ f2 = plot_lateral_profiles(
     uplim=1,
     cuts=cuts_100,
 )
-f2.savefig(f"lateral_profiles_{energy1}MeV{suffix}{fig_extension}")
 
-f3 = compute_voxelDiff(
+fd = plot_lateral_profiles(
+    g4_dose2,
+    sde_dose2,
+    xmin=0,
+    xmax=18,
+    ymin=-10,
+    ymax=10,
+    zmin=-10,
+    zmax=10,
+    lowlim=-1,
+    uplim=1,
+    cuts=cuts_150,
+)
+
+# Dose and difference - 2D
+fe = plot_slice(
+    g4_dose1,
+    sde_dose1,
+    "z",
+    minval=-7,
+    xmin=0,
+    xmax=9.5,
+    ymin=-5,
+    ymax=5,
+    zmin=-5,
+    zmax=5,
+)
+
+ff = compute_voxelDiff(
     g4_dose1,
     sde_dose1,
     xmin=0,
@@ -121,7 +142,30 @@ f3 = compute_voxelDiff(
     zmin=-5,
     zmax=5,
 )
-f3.savefig(f"doseDiff_{energy1}MeV{suffix}{fig_extension}")
+
+fg = plot_slice(
+    g4_dose2,
+    sde_dose2,
+    "z",
+    minval=-7,
+    xmin=0,
+    xmax=18,
+    ymin=-10,
+    ymax=10,
+    zmin=-10,
+    zmax=10,
+)
+
+fh = compute_voxelDiff(
+    g4_dose2,
+    sde_dose2,
+    xmin=0,
+    xmax=18,
+    ymin=-4,
+    ymax=4,
+    zmin=-10,
+    zmax=10,
+)
 
 # Gamma analysis
 print("Gamma values for 100 MeV: ")
@@ -140,49 +184,6 @@ g1, pr1 = pymedphys_gamma(
     zmax=5,
 )
 
-# Individual plots - energy 2
-f4 = plot_slice(
-    g4_dose2,
-    sde_dose2,
-    "z",
-    minval=-5,
-    xmin=0,
-    xmax=18,
-    ymin=-10,
-    ymax=10,
-    zmin=-10,
-    zmax=10,
-)
-f4.savefig(f"2DSlice_z_{energy2}MeV{suffix}{fig_extension}")
-
-f5 = plot_lateral_profiles(
-    g4_dose2,
-    sde_dose2,
-    xmin=0,
-    xmax=18,
-    ymin=-10,
-    ymax=10,
-    zmin=-10,
-    zmax=10,
-    lowlim=-1,
-    uplim=1,
-    cuts=cuts_150,
-)
-f5.savefig(f"lateral_profiles_{energy2}MeV{suffix}{fig_extension}")
-
-f6 = compute_voxelDiff(
-    g4_dose2,
-    sde_dose2,
-    xmin=0,
-    xmax=18,
-    ymin=-4,
-    ymax=4,
-    zmin=-10,
-    zmax=10,
-)
-f6.savefig(f"doseDiff_{energy2}MeV{suffix}{fig_extension}")
-
-# Gamma analysis
 print("Gamma values for 150 MeV: ")
 g2, pr2 = pymedphys_gamma(
     g4_dose2,
@@ -199,11 +200,6 @@ g2, pr2 = pymedphys_gamma(
     zmax=5,
 )
 
-# Comparative plots
-f7, f8 = compare_bragg_peaks(sde_dose1, g4_dose1, energy1, sde_dose2, g4_dose2, energy2)
-f7.savefig(f"1DProj_{energy1}_vs_{energy2}MeV{suffix}{fig_extension}")
-f8.savefig(f"1DSlice_{energy1}_vs_{energy2}MeV{suffix}{fig_extension}")
-
 # Proton range numbers
 r90_1 = range_comparison(g4_dose1, sde_dose1, 0.9)
 r50_1 = range_comparison(g4_dose1, sde_dose1, 0.5)
@@ -216,21 +212,51 @@ print(
     f"For {energy2} MeV, proton range agreement is within {r90_2*10:.2f} mm (R90) and {r50_2*10:.2f} mm (R50)"
 )
 
+# Save figures with corresponding names according to PMB article
+if suffix == "_water":
+    fa.savefig(f"figure2a{fig_extension}")
+    fb.savefig(f"figure2b{fig_extension}")
+    fc.savefig(f"figure3a{fig_extension}")
+    fd.savefig(f"figure3b{fig_extension}")
+    fe.savefig(f"figure4a{fig_extension}")
+    ff.savefig(f"figure4b{fig_extension}")
+    fg.savefig(f"figure4c{fig_extension}")
+    fh.savefig(f"figure4d{fig_extension}")
+if suffix == "_slab":
+    fa.savefig(f"figure6a{fig_extension}")
+    fb.savefig(f"figure6b{fig_extension}")
+    fc.savefig(f"figure7a{fig_extension}")
+    fd.savefig(f"figure7b{fig_extension}")
+    fe.savefig(f"figure8a{fig_extension}")
+    ff.savefig(f"figure8b{fig_extension}")
+    fg.savefig(f"figure8c{fig_extension}")
+    fh.savefig(f"figure8d{fig_extension}")
+if suffix == "_insert":
+    fa.savefig(f"figure9a{fig_extension}")
+    fb.savefig(f"figure9b{fig_extension}")
+    fc.savefig(f"figure10a{fig_extension}")
+    fd.savefig(f"figure10b{fig_extension}")
+    fe.savefig(f"figure11a{fig_extension}")
+    ff.savefig(f"figure11b{fig_extension}")
+    fg.savefig(f"figure11c{fig_extension}")
+    fh.savefig(f"figure11d{fig_extension}")
+
 # Comparison with other physics lists, only available for homogeneous phantom configs:
 if comparePhysics and suffix == "_water":
-    g4_dose_emy1 = retrieve_g4_output(
+    g4_dose_emy1 = MeV_g_to_Gy * retrieve_g4_output(
         f"G4_{N_primaries}_{energy1}MeV{suffix}_emy.root"
-    ) / (float(N_primaries) * density_matrix * voxel_volume)
-    g4_dose_bert1 = retrieve_g4_output(
+    ) / (density_matrix * voxel_volume)
+    g4_dose_bert1 = MeV_g_to_Gy * retrieve_g4_output(
         f"G4_{N_primaries}_{energy1}MeV{suffix}_bert.root"
-    ) / (float(N_primaries) * density_matrix * voxel_volume)
-    g4_dose_emy2 = retrieve_g4_output(
+    ) / (density_matrix * voxel_volume)
+    g4_dose_emy2 = MeV_g_to_Gy * retrieve_g4_output(
         f"G4_{N_primaries}_{energy2}MeV{suffix}_emy.root"
-    ) / (float(N_primaries) * density_matrix * voxel_volume)
-    g4_dose_bert2 = retrieve_g4_output(
+    ) / (density_matrix * voxel_volume)
+    g4_dose_bert2 = MeV_g_to_Gy * retrieve_g4_output(
         f"G4_{N_primaries}_{energy2}MeV{suffix}_bert.root"
-    ) / (float(N_primaries) * density_matrix * voxel_volume)
-    f9 = plot_multiple_bragg_peaks(
+    ) / (density_matrix * voxel_volume)
+
+    fi = plot_multiple_bragg_peaks(
         g4_dose1,
         g4_dose_emy1,
         g4_dose_bert1,
@@ -241,9 +267,8 @@ if comparePhysics and suffix == "_water":
         ref_name="QGSP_BIC_EMZ",
         maxdif=10,
     )
-    f9.savefig(f"1DProj_multiComparison_{energy1}MeV{fig_extension}")
 
-    f10 = plot_multiple_bragg_peaks(
+    fj = plot_multiple_bragg_peaks(
         g4_dose1,
         g4_dose_emy1,
         g4_dose_bert1,
@@ -253,30 +278,32 @@ if comparePhysics and suffix == "_water":
         names=["QGSP_BIC_EMY", "QGSP_BERT", "SDE"],
         ref_name="QGSP_BIC_EMZ",
     )
-    f10.savefig(f"1DSlice_multiComparison_{energy1}MeV{fig_extension}")
 
-    f11 = plot_multiple_bragg_peaks(
-        g4_dose2,
-        g4_dose_emy2,
-        g4_dose_bert2,
-        sde_dose2,
-        how="proj",
-        xmax=17,
-        names=["QGSP_BIC_EMY", "QGSP_BERT", "SDE"],
-        ref_name="QGSP_BIC_EMZ",
-        maxdif=25,
-    )
-    f11.savefig(f"1DProj_multiComparison_{energy2}MeV{fig_extension}")
+    fi.savefig(f"figure5a{fig_extension}")
+    fj.savefig(f"figure5b{fig_extension}")
 
-    f12 = plot_multiple_bragg_peaks(
-        g4_dose2,
-        g4_dose_emy2,
-        g4_dose_bert2,
-        sde_dose2,
-        how="slice",
-        xmax=17,
-        names=["QGSP_BIC_EMY", "QGSP_BERT", "SDE"],
-        ref_name="QGSP_BIC_EMZ",
-        maxdif=30,
-    )
-    f12.savefig(f"1DSlice_multiComparison_{energy2}MeV{fig_extension}")
+    # Uncomment the following lines to plot 1D slices
+    # fk = plot_multiple_bragg_peaks(
+    #     g4_dose2,
+    #     g4_dose_emy2,
+    #     g4_dose_bert2,
+    #     sde_dose2,
+    #     how="proj",
+    #     xmax=17,
+    #     names=["QGSP_BIC_EMY", "QGSP_BERT", "SDE"],
+    #     ref_name="QGSP_BIC_EMZ",
+    #     maxdif=25,
+    # )
+    #
+    # fl = plot_multiple_bragg_peaks(
+    #     g4_dose2,
+    #     g4_dose_emy2,
+    #     g4_dose_bert2,
+    #     sde_dose2,
+    #     how="slice",
+    #     xmax=17,
+    #     names=["QGSP_BIC_EMY", "QGSP_BERT", "SDE"],
+    #     ref_name="QGSP_BIC_EMZ",
+    #     maxdif=30,
+    # )
+
